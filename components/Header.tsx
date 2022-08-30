1a127d66fa5627ffbc8fa299fb4f0ed6bc357f72
planetoida90210 <galeriakoloru90210@gmail.com>
2022-08-30 18:10:45 +0200

    display navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 import { useTheme } from 'next-themes';
+import {
+  SearchIcon,
+  UserIcon,
+  ShoppingBagIcon
+} from '@heroicons/react/outline';
 
 //external imports
 import images from '../assets/app'
@@ -9,12 +15,32 @@ const Header = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <header className="flexBetween">
-      <div className="relative h-20 w-20">
-        <Image src={images.logo} className={theme === 'light' ? 'filter invert' : ''} layout="fill" objectFit="contain" alt="logo"/>
+    <header className="sticky top-0 z-30 flex w-full items-center justify-between p-4 dark:bg-[#0f0f12] bg-[#979797]">
+      <div className="flex items-center justify-center md:w-1/5">
+        <Link href="/">
+        <div className="relative h-[90px] w-[90px] cursor-pointer opacity-75 transition hover:opacity-100">
+          <Image src={images.logo} className={theme === 'light' ? 'filter invert' : ''} layout="fill" objectFit="contain" alt="logo"/>
+        </div>
+      </Link>
       </div>
-      <div className="flex flex-initial flex-row justify-end">
-        <div className="flex items-center mr-2">
+      <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
+        <a className="headerLink">Produkty</a>
+        <a className="headerLink">Nowości</a>
+        <a className="headerLink">Odkrywaj</a>
+        <a className="headerLink">Info</a>
+      </div>
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+        <SearchIcon className="headerIcon"/>
+        <Link href="/checkout">
+          <div className="relative cursor-pointer">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+        <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center ml-2">
           <input
             type="checkbox"
             className="checkbox"
@@ -28,6 +54,10 @@ const Header = () => {
           </label>
         </div>
       </div>
+      </div>
+      
+      
+      
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,17 @@ module.exports = {
       colors: {
         'gk-dark': '#24252D'
       },
-      flex: {
-        2: '2 2 0%'
-      },
-      screens: {
-        lg: { max: '1800px' },
-        md: { max: '990px' },
-        sm: { max: '600px' },
-        xs: { max: '400px' },
-        minmd: '1700px',
-        minlg: '2100px'
-      }
+      // flex: {
+      //   2: '2 2 0%'
+      // },
+      // screens: {
+      //   lg: { max: '1800px' },
+      //   md: { max: '990px' },
+      //   sm: { max: '600px' },
+      //   xs: { max: '400px' },
+      //   minmd: '1700px',
+      //   minlg: '2100px'
+      // }
     },
   },
   plugins: [],
