b595ec7828f16a38f1b656f053ce085738f8959e
planetoida90210 <galeriakoloru90210@gmail.com>
2022-08-31 23:09:27 +0200

    import headless ui tabs create products section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Header, HomePage } from '../components'
+import { Tab } from '@headlessui/react';
+import { getEnabledCategories } from 'trace_events';
 
 const Home: NextPage = () => {
   return (
@@ -11,9 +13,41 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className="relative h-[200vh] dark:bg-[#0f0f12] bg-[#E7ECEE]">
         <HomePage />
       </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen dark:bg-[#24252D] bg-white">
+        <div className="space-y-10 py-16">
+          <h1 className="text-center text-4xl font-medium tracking-wide md:text-5xl">
+           Produkty
+          </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {/* {getEnabledCategories.map((category)=>(
+                <Tab
+                key={category._id}
+                id={category._id}
+                className={({selected}) =>
+                `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                  selected
+                  ? "borderGradient bg-[#35383C] text-white"
+                  : "border-b-2 border-[#35383C] text-[#747474]"
+                }`
+              }
+                >
+                  {category.title}
+                </Tab>
+              ))} */}
+            </Tab.List>
+            <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </section>
     </div>
   )
 }
