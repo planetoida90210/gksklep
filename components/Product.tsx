5f7d675ee09e7c5a1c03c4664eb172e36e394065
planetoida90210 <galeriakoloru90210@gmail.com>
2022-09-02 21:29:19 +0200

    display info on product card

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,3 +1,4 @@
+import { ShoppingCartIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
 import { urlFor } from '../sanity'
@@ -11,6 +12,15 @@ const Product = ({product}: Props) => {
     <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
       <div className="relative h-64 w-full md:h-72">
         <Image src={urlFor(product.image[0]).url()} layout="fill" objectFit="contain"/>
+      </div>
+      <div className="flex flex-1 items-center justify-between space-x-3">
+        <div className="space-y-2 text-xl text-white md:text-2xl">
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+      <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-violet-500 to-blue-500">
+        <ShoppingCartIcon className="h-8 w-8 text-white"/>
+      </div>
       </div>
     </div>
   )
