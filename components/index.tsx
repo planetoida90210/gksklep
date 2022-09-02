2856a3db228fb47384b4e3216db524c991175e18
planetoida90210 <galeriakoloru90210@gmail.com>
2022-09-03 00:02:37 +0200

    create product details structure

--- components/index.tsx
+++ components/index.tsx
@@ -2,5 +2,6 @@ import Header from './Header';
 import HomePage from './HomePage';
 import Button from './Button';
 import Product from './Product';
+import ProductDetail from './ProductDetail';
 
-export {Header, Product, HomePage, Button};
+export {Header, Product, HomePage, Button, ProductDetail};
