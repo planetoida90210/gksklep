87cd89773df77d7ec382f84dae39cfd2f4d74901
planetoida90210 <galeriakoloru90210@gmail.com>
2022-09-04 13:15:23 +0200

    create selecting basket item with id, select total basket amount slices in redux basketSlice

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -43,5 +43,11 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors
 export const selectBasketItems = (state: RootState) => state.basket.items;
+export const selectBasketItemsWithId = (state: RootState, id: string) => {
+  state.basket.items.filter((item: Product) => item._id === id);
+};
+export const selectBasketTotal = (state: RootState) => {
+  state.basket.items.reduce((total: number, item: Product) => (total += item.price), 0);
+}
 
 export default basketSlice.reducer
