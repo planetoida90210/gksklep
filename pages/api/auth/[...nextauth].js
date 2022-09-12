8f6bd1ef289ee637fb158583aee4d866f911e097
planetoida90210 <galeriakoloru90210@gmail.com>
2022-09-12 23:37:21 +0200

    add next-auth bolierplate

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google';
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  ]
+})
